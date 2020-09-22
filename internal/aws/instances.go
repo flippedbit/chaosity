8f9da13a95a65a81550b8d0d60dba9800be25a41
flippedbit <straughan.michael@gmail.com>
2020-09-21 23:38:47 -0400

    adding some comments to internal AWS functions

--- internal/aws/instances.go
+++ internal/aws/instances.go
@@ -10,11 +10,12 @@ import (
 )
 
 // GetInstancesBySubnet gathers all Instance objects from a list of subnets provided
-// it will separate out multiple subnets with a comma (,) delimiter
+// it will separate out multiple subnets with a comma (,) delimiter.
 // Returns a list of ec2.Instances and an error
 func GetInstancesBySubnet(svc *ec2.EC2, s *string) ([]*ec2.Instance, error) {
 	var instancesList []*ec2.Instance
 	subnets := strings.Split(*s, ",")
+	// cycle through given subnet IDs and gather instances from all the subnets
 	for _, subnet := range subnets {
 		input := &ec2.DescribeInstancesInput{
 			Filters: []*ec2.Filter{
@@ -26,6 +27,7 @@ func GetInstancesBySubnet(svc *ec2.EC2, s *string) ([]*ec2.Instance, error) {
 				},
 			},
 		}
+		// AWS describe on all instances with AWS error handling
 		instancesOutput, err := svc.DescribeInstances(input)
 		if err != nil {
 			if aerr, ok := err.(awserr.Error); ok {
@@ -37,6 +39,7 @@ func GetInstancesBySubnet(svc *ec2.EC2, s *string) ([]*ec2.Instance, error) {
 				return instancesList, fmt.Errorf(err.Error())
 			}
 		}
+		// cycle through instances and gather instances in order to return a list of ec2.Instance
 		for _, reservation := range instancesOutput.Reservations {
 			for _, instance := range reservation.Instances {
 				instancesList = append(instancesList, instance)
@@ -46,17 +49,20 @@ func GetInstancesBySubnet(svc *ec2.EC2, s *string) ([]*ec2.Instance, error) {
 	return instancesList, nil
 }
 
-// ApplyChaosSecurityGroupToInstances applies given SecurityGroup ID (sg) to all instances provided in the ec2.Instance list (instances)
+// ApplyChaosSecurityGroupToInstances applies given SecurityGroup ID (sg) to all instances provided in the ec2.Instance list (instances).
 // Returns an error
 func ApplyChaosSecurityGroupToInstances(svc *ec2.EC2, instances []*ec2.Instance, sg string) error {
+	// cycle through given ec2.Instance list
 	for _, i := range instances {
 		fmt.Println("Applying SecurityGroups ", sg, " to instance ", *i.InstanceId)
+		// modify every instance with the new segurity group ID for the chaos deny sg that was created and passed
 		input := &ec2.ModifyInstanceAttributeInput{
 			InstanceId: aws.String(*i.InstanceId),
 			Groups: []*string{
 				aws.String(sg),
 			},
 		}
+		// apply instance changes with AWS error handling
 		if _, err := svc.ModifyInstanceAttribute(input); err != nil {
 			if aerr, ok := err.(awserr.Error); ok {
 				switch aerr.Code() {
@@ -71,19 +77,23 @@ func ApplyChaosSecurityGroupToInstances(svc *ec2.EC2, instances []*ec2.Instance,
 	return nil
 }
 
-// RevertChaosSecurityGroupOnInstances applies the original SecurityGroups to each ec2.Instance within the list (instances)
+// RevertChaosSecurityGroupOnInstances applies the original SecurityGroups to each ec2.Instance within the list (instances).
 // Returns error
 func RevertChaosSecurityGroupOnInstances(svc *ec2.EC2, instances []*ec2.Instance) error {
+	// cycle through given ec2.Instance list
 	for _, i := range instances {
 		var originalSecurityGroups []*string
+		// cycle through grabbing all original security groups for each instance
 		for _, s := range i.SecurityGroups {
 			fmt.Println("Applying SecurityGroups ", *s.GroupId, " to instance ", *i.InstanceId)
 			originalSecurityGroups = append(originalSecurityGroups, s.GroupId)
 		}
+		// create the new instance attribute object to be applied combining the instanceID and securitygroupID's
 		input := &ec2.ModifyInstanceAttributeInput{
 			InstanceId: aws.String(*i.InstanceId),
 			Groups:     originalSecurityGroups,
 		}
+		// apply the original securitygroups to each instance with AWS error handling
 		if _, err := svc.ModifyInstanceAttribute(input); err != nil {
 			if aerr, ok := err.(awserr.Error); ok {
 				switch aerr.Code() {
@@ -98,16 +108,21 @@ func RevertChaosSecurityGroupOnInstances(svc *ec2.EC2, instances []*ec2.Instance
 	return nil
 }
 
+// RebootInstances reboots all instances given in an ec2.Instance list (instances).
+// Returns an error
 func RebootInstances(svc *ec2.EC2, instances []*ec2.Instance) error {
 	var iList []*string
+	// cycle through given ec2.Instance list gathering their InstanceID into a list of pointers []*string
 	for _, i := range instances {
 		fmt.Println("Rebooting instance ", *i.InstanceId)
 		iList = append(iList, i.InstanceId)
 	}
 
+	// create the AWS structure of the instances to be rebooted
 	input := &ec2.RebootInstancesInput{
 		InstanceIds: iList,
 	}
+	// reboot all the instanceID's and check for AWS errors
 	_, err := svc.RebootInstances(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
--- internal/aws/securitygroups.go
+++ internal/aws/securitygroups.go
@@ -8,14 +8,16 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-// GenerateDenySecurityGroup used to create an emtpy SecurityGroup within the AWS VPC given
+// GenerateDenySecurityGroup used to create an emtpy SecurityGroup within the AWS VPC given.
 // Returns SecurityGroup-ID as a string and an error
 func GenerateDenySecurityGroup(svc *ec2.EC2, vpc *string) (string, error) {
+	// generate default template for a security group, by default will deny all ingress and permit all egress
 	groupInput := &ec2.CreateSecurityGroupInput{
 		Description: aws.String("Deny security group used for chaos testing"),
 		GroupName:   aws.String("chaos-deny-security-group"),
 		VpcId:       vpc,
 	}
+	// create the security group and check for AWS errors
 	groupResult, err := svc.CreateSecurityGroup(groupInput)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
@@ -30,6 +32,11 @@ func GenerateDenySecurityGroup(svc *ec2.EC2, vpc *string) (string, error) {
 	}
 	sgID := *groupResult.GroupId
 
+	// describe the newly created security group so we can make sure to remove the exact egress IpPermissionsEgress late on
+	/**
+	 * @todo
+	 * @body create a generic SecurityGroup.IpPermissionsEgress structure so we do not need to make an additoinal api call
+	 */
 	describeGroupInput := &ec2.DescribeSecurityGroupsInput{
 		GroupIds: []*string{
 			aws.String(sgID),
@@ -48,10 +55,12 @@ func GenerateDenySecurityGroup(svc *ec2.EC2, vpc *string) (string, error) {
 	}
 	sgIPPermissionsEgress := describeGroupResult.SecurityGroups[0].IpPermissionsEgress
 
+	// build struct to remove default egress rule from generated security group
 	revokeSecurityGroupInput := &ec2.RevokeSecurityGroupEgressInput{
 		GroupId:       aws.String(sgID),
 		IpPermissions: sgIPPermissionsEgress,
 	}
+	// remove egress rule from security group and check for AWS errors
 	if _, err = svc.RevokeSecurityGroupEgress(revokeSecurityGroupInput); err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
@@ -66,12 +75,14 @@ func GenerateDenySecurityGroup(svc *ec2.EC2, vpc *string) (string, error) {
 	return sgID, nil
 }
 
-// DeleteDenySecurityGroup removes the given SecurityGroup ID from AWS
+// DeleteDenySecurityGroup removes the given SecurityGroup ID from AWS.
 // Returns an error
 func DeleteDenySecurityGroup(svc *ec2.EC2, sg string) error {
+	// create struct to for deleting the originally created security group
 	input := &ec2.DeleteSecurityGroupInput{
 		GroupId: aws.String(sg),
 	}
+	// delete the originally created security group and check for AWS errors
 	_, err := svc.DeleteSecurityGroup(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
